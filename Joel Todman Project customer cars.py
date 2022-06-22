1298507fd569bfcc0f88a0aaf71bc9fec39adb8e
Joel Todman <107224292+jtodman9366@users.noreply.github.com>
2022-06-22 00:01:09 +0000

    finish project

---
+++ Joel Todman Project customer cars.py
@@ -0,0 +1,57 @@
+print("Project: Custom Cars")
+print("========================")
+print("Welcometo the UMBC\nCar Customization Form!")
+print("========================")
+
+# Make & Model Selection 
+print("1. Select your car mske & model choice ")
+print("  a.dodge")
+print("  b.toyota")
+print("  c.lexus")
+print("  d.camary")
+choice  = input("Please Enter 'a' - 'd' ")
+
+#Upgrade Options
+print()
+print("2. Would you like to upgrade from  4-door sedan w/o sunroof to a 2-doorsedan with sunroof")
+upgrade  = input("Please Enter 'yes' or 'no' ")
+
+#Car Exterior
+print()
+print("3. What color would you like the exterior of your car? ")
+ExteriorColor = input("Please Enter your exterior color you would like: ")
+
+#Deluxe Package
+print()
+print("4. Would you like the Deluxe weather package? ")
+deluxePackage  = input("Please Enter 'yes' or 'no' ")
+
+#Car Engine
+print()
+print("5. What type of Engine would you like?")
+print("  a.I-4 Entry Engine")
+print("  b.V-6 Performance Engine")
+print("  c.ECO Deisel Engine")
+engineType = input("Please Enter 'a' - 'c' ")
+
+#Heated Seats
+print()
+print("6. Would you like the Heated Seats option? ")
+heatedSeats  = input("Please Enter 'yes' or 'no' ")
+print()
+print("========================")
+print("~ Summary~ ")
+print(f"Make & Model Option: {choice}\nUpgrade to 2-door Sedan With Sunroof: {upgrade}\nExterior Color: {ExteriorColor}\nDeluxe Weather Package: {deluxePackage}\nEngine Option: {engineType}\nHeated Seats: {heatedSeats}")
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+
--- Joel_Todman-project:Story Time.py
+++ Joel_Todman-project:Story Time.py
@@ -5,16 +5,15 @@ print("You are a knight in the year 650 CE, and you are trying to become the mos
 print()
 print("You decided to go on an adventure at castle UMBC to earn your glory!")
 print()
-# The Blacksmith's Story...Make Your Selection: 
+# Make Your Selection: 
 print("You are outside the castle grounds, and see three different people asking for help, you can talk to:")
 print("A. The local Blacksmith, who needs a rare sword ")
 print("B. The town's Teacher, who has a question ")
 print("C. The King, who needs a brave warrior ")
 selection  = input("Who will you talk to, Pleaes select  option 'A' - 'C': ")
 if(selection == "A"):
-  print()
   print("you meet with the town Blacksmith, looking for legendary sword")
-  print("They tell you the legen that the sword is frozen meet with the ton Blacksmith, lookin ice waiting to be found.")
+  print("They tell you the legen that the sword is frozen meet with the town Blacksmith, lookin ice waiting to be found.")
   print("You choose to search for it: ")
   print("A. In the Desert ")
   print("B. In the Forest ")
@@ -24,12 +23,12 @@ print()
 if(search == "C"):
   print("After looking long and hard through the arctic, you.ve found it -  the legendary sword of programming. The Blacksmith is thrilled and reward you with a suit of armor worthy of a hero\n ~ The End ~  ")
 print()
-# The Blacksmoith's Story...Make Your Selection: 
-if(search  == "A" or  search =="B" ):
+# Make Your Selection: A, B, C
+if(search  == "A" or search =="B" ):
   print("You serached and searched for the rest of your life, but never found it.It might've have been a good idea to head the blacksmith advice and go to a cold place.\n ~ The End ~  ")
   
-# The Teachers Story...Make Your Selection:
-print()
+# Make Your Selection:
+
 print("You meet with the town teacher, who is asking you a question, 'I have to be honest with you ,' they say. 'I am not really qualified to teach and strugglin with this question 'They gesture to the math equation that reads as follow:\n\n y = 6 + 2 + 1 ") 
 print()
 y = input("please enter the solution to this problem: ") 
@@ -39,8 +38,10 @@ if(y == 9):
   print()
 print("That makes perfect sense' the teacher cries and they award you with an honary degree. You are forever known as one of the smartest in the land!\n~ The End ~ ")
 if(y != 9):
-  print("Yankee Doodle")
-#The King's Story...Make your selection
+  print()
+print("Yankee Doodle")
+
+#The King's Story
 print()
 print("You are outside the castle groundschoose to search , and see three differe; ")
 print("A. The local Blacksmith, who needs a rare sword ")
